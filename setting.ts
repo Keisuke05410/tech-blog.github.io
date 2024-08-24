b4ff395b783a436c7a92618af9464917fc34d870
Keisuke05410 <keisuke3469@gmail.com>
2024-08-24 21:07:29 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://keisuke05410.github.io/tech-blog.github.io/";
